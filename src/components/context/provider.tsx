2d86af8fa39537ade1fc8fe22d779221aa6e20e6
pablo-peek <pablovargas72h@gmail.com>
2024-11-28 13:50:29 -0600

    feat: update token validation request to include empty body in AuthProvider

--- src/components/context/provider.tsx
+++ src/components/context/provider.tsx
@@ -33,11 +33,15 @@ const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
 
       if (token && user) {
         const [error, response] = await to(
-          axios.post(`${process.env.REACT_APP_ROBUST_API_BASE_URL}/auth/validate-token`, {
-            headers: {
-              Authorization: token,
-            },
-          })
+          axios.post(
+            `${process.env.REACT_APP_ROBUST_API_BASE_URL}/auth/validate-token`,
+            {},
+            {
+              headers: {
+                Authorization: token, 
+              },
+            }
+          )
         );
 
         if (error) {
